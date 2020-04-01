d95b1c3c98475d0cec2b532870f6d1b715ba4ce6
unknown <awesomeriptide@gmail.com>
2020-04-01 12:24:54 -0400

    "target\debug\shove.exe testing next build"

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{env, process::Command};
+use std::{
+    env,
+    process::{Command, Output},
+    str,
+};
 fn main() {
     let empty_string = "".to_owned();
     let mut commit_message = env::args().collect::<Vec<String>>().join(" ");
@@ -6,25 +10,29 @@ fn main() {
         commit_message =
             "Pushed with ~~love~~ [shove](https://github.com/OriontheCat/shove)".to_owned();
     }
-    println!(
-        "{:?}",
-        Command::new("git")
-            .args(&["add", "."])
-            .output()
-            .expect("failed to `git add .`")
-    );
-    println!(
-        "{:?}",
-        Command::new("git")
-            .args(&["commit", "-m", &*format!("\"{}\"", commit_message)])
-            .output()
-            .expect(&*format!("failed to `commit -m {}`", commit_message)),
-    );
-    println!(
-        "{:?}",
-        Command::new("git")
-            .args(&["push",])
-            .output()
-            .expect("failed to `git push`"),
-    );
+    let output = Command::new("git")
+        .args(&["add", "."])
+        .output()
+        .expect("failed to `git add .`");
+    print_output(output);
+    let output = Command::new("git")
+        .args(&["commit", "-m", &*format!("\"{}\"", commit_message)])
+        .output()
+        .expect(&*format!("failed to `commit -m {}`", commit_message));
+    print_output(output);
+    let output = Command::new("git")
+        .args(&["push"])
+        .output()
+        .expect("failed to `git push`");
+    print_output(output);
+}
+
+fn print_output(output: Output) {
+    let Output {
+        stdout,
+        stderr,
+        status,
+    } = output;
+    println!("{}", str::from_utf8(&stdout).expect("stdout malformed"));
+    eprintln!("{}", str::from_utf8(&stderr).expect("stderr malformed"));
 }
