6f57eacb339be6dcee9bbbda3b1bf08009c2d0a5
unknown <awesomeriptide@gmail.com>
2020-04-01 12:28:05 -0400

    "target\debug\shove.exe"

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let empty_string = "".to_owned();
     let mut commit_message = env::args().collect::<Vec<String>>().join(" ");
     if commit_message == empty_string {
+        println!("empty string");
         commit_message =
             "Pushed with ~~love~~ [shove](https://github.com/OriontheCat/shove)".to_owned();
     }
