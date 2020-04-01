769992c0ca58710343195e3b785f3c92f4557ca4
unknown <awesomeriptide@gmail.com>
2020-04-01 12:15:17 -0400

    "target\debug\shove.exe testing intitial build"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-use std::env;
+use std::{env, process::Command};
 fn main() {
-    for argument in env::args() {
-        println!("{}", argument);
+    let empty_string = "".to_owned();
+    let mut commit_message = env::args().collect::<Vec<String>>().join(" ");
+    if commit_message == empty_string {
+        commit_message =
+            "Pushed with ~~love~~ [shove](https://github.com/OriontheCat/shove)".to_owned();
     }
+    println!(
+        "{:?}",
+        Command::new("git")
+            .args(&["add", "."])
+            .output()
+            .expect("failed to `git add .`")
+    );
+    println!(
+        "{:?}",
+        Command::new("git")
+            .args(&["commit", "-m", &*format!("\"{}\"", commit_message)])
+            .output()
+            .expect(&*format!("failed to `commit -m {}`", commit_message)),
+    );
+    println!(
+        "{:?}",
+        Command::new("git")
+            .args(&["push",])
+            .output()
+            .expect("failed to `git push`"),
+    );
 }
